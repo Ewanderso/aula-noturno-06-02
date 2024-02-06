e37bc64851b24517bfd22cde5234f0c4accbd763
Ewanderson <151974780+Ewanderso@users.noreply.github.com>
2024-02-06 18:59:11 -0300

    Add files via upload

---
+++ cadastro_aluno.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Alunos</title>
+</head>
+<body>
+    Cadastro Alunos</p>
+
+    <form method="post" action="incluir_aluno.php">
+        <label for="nome">Nome:</label><br>
+        <input type="text" id="nome" name="nome"><br>
+        <label for="email">e-mail:</label><br>
+        <input type="text" id="email" name="email"><br>
+        <label for="telefone">Telefone:</label><br>
+        <input type="text" id="telefone" name="telefone"><br>
+        <label for="responsavel">Responsavel:</label><br>
+        <input type="text" id="responsavel" name="responsavel"><br>
+        <input type="submit" value="Enviar"></p>
+    </form>
+
+</body>
+</html>
